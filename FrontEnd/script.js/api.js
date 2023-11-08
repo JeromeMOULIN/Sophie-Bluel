498c15f2a1d129b832422307d38c7ea17332e2a5
JeromeMOULIN <j.moulin89@laposte.net>
2023-11-08 15:17:08 +0100

    Les filtres son maintenant ajouter dynamiquement

--- FrontEnd/script.js/api.js
+++ FrontEnd/script.js/api.js
@@ -1,3 +1,10 @@
+// recuperer les catégorie
+const getCategory = async () => {
+    const response = await fetch('http://localhost:5678/api/categories')
+    const categorys = await response.json();
+    return {categorys}
+}
+
 //recuperer de l'api
 const getAllWorks =  async () => {
     const response = await fetch('http://localhost:5678/api/works')
@@ -5,45 +12,18 @@ const getAllWorks =  async () => {
     
     //création du template pour mes projet
     let templateProject = '';
+    let templateModal = '';
     for (const work of works) {
         templateProject = templateProject + `<figure id="${work.categoryId}" data-pictureId="${work.id}" class="show">
                                             <img src="${work.imageUrl}" alt="${work.title}">
                                             <figcaption>${work.title}</figcaption>
                                             </figure>`;
-    }
-    let templateModal = '';
-    for (const work of works) {
         templateModal = templateModal + `<figure id="${work.id}" class="adminWorks">
                                         <img src="${work.imageUrl}" alt="${work.title}">
                                         <button class="trash"><i class="fa-solid fa-trash-can"></i></button>
                                         </figure>`;
     }
     return {templateProject, templateModal}
-}
-
-
-//recupération des filtres
-let filters = document.querySelectorAll("#filters button");
-//pour chaque filtre de mes filtres j'écoute le click
-
-for (let filter of filters) {
-    filter.addEventListener("click", () => {
-        // Pour chaqu'un de met filtre je remplace la class filtreSelected si presente par [...]Unselected
-        filters.forEach((filter) => filter.classList.replace("filterSelected", "filterUnselected"))
-        // Je change la classe de l'actuel bouton cliquer par filterSelected
-        filter.classList.replace("filterUnselected", "filterSelected")
-        //Récupération de toute mes cartes
-        let cards = document.querySelectorAll(".gallery figure")
-        //Pour chaque carte de mes cartes
-        for (let card of cards) {
-            // je cache tout les cartes
-            card.classList.replace("show", "hide")
-            if (card.id === filter.id || filter.id === "all") {
-                //j'affiche toute les cartes dont l'id catégorie est egale a l'id du filtre
-                card.classList.replace("hide", "show")
-            }
-        }
-    })
 }
 //Je recupere le bouton d'ajout de contenue
 const bntSubmit = document.getElementById('btnAddContent')
--- FrontEnd/script.js/index.js
+++ FrontEnd/script.js/index.js
@@ -1,4 +1,36 @@
 
+getCategory().then(categorys => {
+    let category = categorys.categorys
+    let categoTemplate = `<button id="all" class="all filterSelected">Tous</button>`;
+    for (const categorie of category){
+        categoTemplate = categoTemplate + `<button id="${categorie.id}" class="all filterUnselected">${categorie.name}</button>`;
+    }
+    document.querySelector('#filters').insertAdjacentHTML('beforeend', categoTemplate)
+
+    //recupération des filtres
+    let filters = document.querySelectorAll("#filters button");
+    //pour chaque filtre de mes filtres j'écoute le click
+
+    for (let filter of filters) {
+    filter.addEventListener("click", () => {
+        // Pour chaqu'un de met filtre je remplace la class filtreSelected si presente par [...]Unselected
+        filters.forEach((filter) => filter.classList.replace("filterSelected", "filterUnselected"))
+        // Je change la classe de l'actuel bouton cliquer par filterSelected
+        filter.classList.replace("filterUnselected", "filterSelected")
+        //Récupération de toute mes cartes
+        let cards = document.querySelectorAll(".gallery figure")
+        //Pour chaque carte de mes cartes
+        for (let card of cards) {
+            // je cache tout les cartes
+            card.classList.replace("show", "hide")
+            if (card.id === filter.id || filter.id === "all") {
+                //j'affiche toute les cartes dont l'id catégorie est egale a l'id du filtre
+                card.classList.replace("hide", "show")
+            }
+        }
+    })
+}
+})
 getAllWorks().then(template => {  
     //injection du template dans mes projets 
     document.querySelector('.gallery').insertAdjacentHTML("beforeend", template.templateProject);
