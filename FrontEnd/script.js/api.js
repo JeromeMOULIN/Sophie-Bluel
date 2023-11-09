a8308a29c22b99c52dd9d5e8c6a4513287070ed4
JeromeMOULIN <j.moulin89@laposte.net>
2023-11-09 14:48:28 +0100

    reconstruction du fetch dajout de work pour transferer lHTML dans lindex.js

--- FrontEnd/script.js/api.js
+++ FrontEnd/script.js/api.js
@@ -6,81 +6,31 @@ const getCategory = async () => {
     return {categorys}
 }
 //recuperer de l'api
+
 const getAllWorks =  async () => {
     const response = await fetch('http://localhost:5678/api/works')
     const worksResponse = await response.json();
     
     return {worksResponse}
 }
-//Je recupere le bouton d'ajout de contenue
-const bntSubmit = document.getElementById('btnAddContent')
-//J'ecoute le click du bouton ajout de contenue
-bntSubmit.addEventListener('click', (e) => {
-    //Je previen le comportement par defaut du formulaire
-    e.preventDefault()
 
-    //Je recupere tout les information du formulaire qui sont a transmettre dans le fetch
-    let image = document.getElementById('pictureLoaded').files[0]
-    let url = URL.createObjectURL(image)
-
-    let picture = document.getElementById("imageUploaded")
-    
-    let title = document.getElementById('workTitle').value
-    
-    let category = document.getElementById('categoryOptions').value
-    
+//ajout de work
+const addWorks = async (formData)  => {
     // Je recupere le token
-    const user = JSON.parse(localStorage.getItem('user')) 
+    const user = JSON.parse(localStorage.getItem('user'))
 
     let token = user.token
 
+    let myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Bearer ' + token);
 
-    if (image === '' || title === '' || category === 'null') {
-        console.log('error')
-    } else {
-        let myHeaders = new Headers();
-        myHeaders.append('Authorization', 'Bearer ' + token );
-
-        let formData = new FormData();
-        formData.append("image", image);
-        formData.append("title", title);
-        formData.append("category", category);
-
-        // on set les options de notre requete fetch
-        let requestOptions = {
+    let requestOptions = {
             method: 'POST',
             headers: myHeaders,
             body: formData,
-        }
-        
-        fetch("http://localhost:5678/api/works", requestOptions)
-            // Convertie la reponse en JSON
-            .then(response => response.json())
-            // Traitement de la reponse
-            .then(data => {
-                //reset du formulaire
-                let templateAddModal = `<figure id="${data.id}"  class="adminWorks">
-                    <img src="${url}" alt="${title}">
-                    <button class="trash"><i class="fa-solid fa-trash-can"></i></button>
-                    </figure>`;
-                  
-                let templateAddHome = `<figure id="${category}" data-pictureId="${data.id}" class="show ">
-                    <img src="${url}" alt="${title}">
-                    <figcaption>${title}</figcaption>
-                    </figure>`;
-                  
-                document.querySelector("#adminGallery").insertAdjacentHTML("beforeend", templateAddModal)
-                document.querySelector(".gallery").insertAdjacentHTML("beforeend", templateAddHome)
-                document.querySelector("#adminGallery").lastElementChild.lastElementChild.addEventListener('click', workDelete)
-                document.querySelector(".formAddWorks").reset()
-                document.getElementById('pictureLoaded').classList.remove('hiddenModalPart')
-                document.querySelector('.addImgLoader i').classList.remove('hiddenModalPart')
-                document.querySelector('.addImgLoader p').classList.remove('hiddenModalPart')
-                document.getElementById('btnAddContent').classList.replace('accessible','inaccessible')
-                document.getElementById('btnAddContent').setAttribute('disabled', true);
-                picture.src = "#"
-                })
-            .catch(error => console.log('error', error));
     }
-})
+    const reponse = await fetch('http://localhost:5678/api/works', requestOptions)
+    const data = reponse.json()
+    return data;
+}
 
--- FrontEnd/script.js/index.js
+++ FrontEnd/script.js/index.js
@@ -3,15 +3,15 @@ getCategory().then(categorys => {
     let category = categorys.categorys
     let categoTemplateDOM = `<button id="all" class="all filterSelected">Tous</button>`;
     let categoInputModal = ``
-    for (const categorie of category){
+    for (const categorie of category) {
         categoTemplateDOM = categoTemplateDOM + `<button id="${categorie.id}" class="all filterUnselected">${categorie.name}</button>`;
-        categoInputModal =  categoInputModal + `<option value="${categorie.id}">${categorie.name}</option>`
+        categoInputModal = categoInputModal + `<option value="${categorie.id}">${categorie.name}</option>`
     }
 
     // injection des filtres dans le DOM
-    if(localStorage.getItem("user") != null){
+    if (localStorage.getItem("user") != null) {
         document.querySelector('#categoryOptions').insertAdjacentHTML('beforeend', categoInputModal)
-    }else{
+    } else {
         document.querySelector('#filters').insertAdjacentHTML('beforeend', categoTemplateDOM)
     }
 
@@ -19,30 +19,29 @@ getCategory().then(categorys => {
     //pour chaque filtre de mes filtres j'écoute le click
 
     for (let filter of filters) {
-    filter.addEventListener("click", () => {
-        // Pour chaqu'un de met filtre je remplace la class filtreSelected si presente par [...]Unselected
-        filters.forEach((filter) => filter.classList.replace("filterSelected", "filterUnselected"))
-        // Je change la classe de l'actuel bouton cliquer par filterSelected
-        filter.classList.replace("filterUnselected", "filterSelected")
-        //Récupération de toute mes cartes
-        let cards = document.querySelectorAll(".gallery figure")
-        //Pour chaque carte de mes cartes
-        for (let card of cards) {
-            // je cache tout les cartes
-            card.classList.replace("show", "hide")
-            if (card.id === filter.id || filter.id === "all") {
-                //j'affiche toutes les cartes dont l'id catégorie est egale a l'id du filtre
-                card.classList.replace("hide", "show")
+        filter.addEventListener("click", () => {
+            // Pour chaqu'un de met filtre je remplace la class filtreSelected si presente par [...]Unselected
+            filters.forEach((filter) => filter.classList.replace("filterSelected", "filterUnselected"))
+            // Je change la classe de l'actuel bouton cliquer par filterSelected
+            filter.classList.replace("filterUnselected", "filterSelected")
+            //Récupération de toute mes cartes
+            let cards = document.querySelectorAll(".gallery figure")
+            //Pour chaque carte de mes cartes
+            for (let card of cards) {
+                // je cache tout les cartes
+                card.classList.replace("show", "hide")
+                if (card.id === filter.id || filter.id === "all") {
+                    //j'affiche toutes les cartes dont l'id catégorie est egale a l'id du filtre
+                    card.classList.replace("hide", "show")
+                }
             }
-        }
-    })
-}
+        })
+    }
 })
 
-getAllWorks().then(worksResponse => { 
+getAllWorks().then(worksResponse => {
     //création du template pour mes projet
     let works = worksResponse.worksResponse
-    console.log(works)
     let templateProject = '';
     let templateModal = '';
     for (const work of works) {
@@ -62,33 +61,33 @@ getAllWorks().then(worksResponse => {
     let delBtn = document.querySelectorAll('.trash')
     delBtn.forEach(trash => {
         trash.addEventListener('click', workDelete)
-        }) 
+    })
 })
 
 // Fonction pour delete les works
-const workDelete = function(){
+const workDelete = function () {
     // Je recupere l'id de l'element parent qui a été clické (le bouton corbeil)
     let workDom = this.parentElement
     let workId = workDom.id
 
-    const user = JSON.parse(localStorage.getItem('user')) 
+    const user = JSON.parse(localStorage.getItem('user'))
     let token = user.token
-            
+
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer "+token);
+    myHeaders.append("Authorization", "Bearer " + token);
 
     var requestOptions = {
         method: 'DELETE',
         headers: myHeaders,
     };
-            
+
     fetch(`http://localhost:5678/api/works/${workId}`, requestOptions)
-    .then(response => {
-        response.text()
-        workDom.remove()
-        document.querySelector(`[data-pictureid='${workId}']`).remove()
-    })
-    .catch(error => console.log('error', error));
+        .then(response => {
+            response.text()
+            workDom.remove()
+            document.querySelector(`[data-pictureid='${workId}']`).remove()
+        })
+        .catch(error => console.log('error', error));
 }
 // Modification si connecté
 // Banner EditorOn
@@ -96,23 +95,23 @@ bannerTemplate = `<div id="editbanner">
     <i class="fa-regular fa-pen-to-square fa-xs"></i>
     <p>Mode edition</p>
     </div>`;
-if(localStorage.getItem("user") != null){
+if (localStorage.getItem("user") != null) {
     document.body.insertAdjacentHTML("afterbegin", bannerTemplate);
 }
 // Login/logout
 loginTemplate = `<li id="login"><a href="./logger.html">login</a></li>`
 logoutTemplate = `<li id="logout"><a href="#">logout</a></li>`
-if(localStorage.getItem("user") != null){
+if (localStorage.getItem("user") != null) {
     document.getElementById("contactNav").insertAdjacentHTML("afterend", logoutTemplate);
     document.getElementById("logout").addEventListener("click", () => {
         localStorage.removeItem("user")
-        document.location.href="./index.html"
+        document.location.href = "./index.html"
     })
-}else{
+} else {
     document.getElementById("contactNav").insertAdjacentHTML("afterend", loginTemplate);
 }
 // bouton modif
-if(localStorage.getItem("user") != null){
+if (localStorage.getItem("user") != null) {
     modifierButton = `<a id="modifButton" class="call-modal" href="#modal1">
         <i class="fa-regular fa-pen-to-square fa-xs"></i>
         <p>Modifier</p>
@@ -137,8 +136,8 @@ const openModal = function (e) {
 }
 
 // Ferme la modal
-const closeModal = function (e){
-    if(modal === null) return
+const closeModal = function (e) {
+    if (modal === null) return
     e.preventDefault()
     modal.style.visibility = 'hidden'
     modal.setAttribute('aria-hidden', 'true')
@@ -149,14 +148,14 @@ const closeModal = function (e){
     modal = null
 }
 
-const stopPropagation = function (e){
+const stopPropagation = function (e) {
     e.stopPropagation()
 }
 
 // Ecoute les touches du clavier qui sont pressé
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', function (e) {
     // Ferme la modal si les touche echape est pressé
-    if (e.key === "Escape" || e.key === "Esc"){
+    if (e.key === "Escape" || e.key === "Esc") {
         closeModal(e)
     }
 })
@@ -178,13 +177,63 @@ document.getElementById('modalArrow').addEventListener('click', () => {
 
 //preview image
 let image = document.getElementById("imageUploaded")
-let previewPicture = function (e){
+let previewPicture = function (e) {
     const [picture] = e.files
 
-    if(picture){
+    if (picture) {
         image.src = URL.createObjectURL(picture)
     }
 }
+//Je recupere le bouton d'ajout de contenue
+const bntSubmit = document.getElementById('btnAddContent')
+//J'ecoute le click du bouton ajout de contenue
+bntSubmit.addEventListener('click', async (e) => {
+    //Je previen le comportement par defaut du formulaire
+    e.preventDefault()
+
+    //Je recupere tout les information du formulaire qui sont a transmettre dans le fetch
+    let image = document.getElementById('pictureLoaded').files[0]
+    let url = URL.createObjectURL(image)
+
+    let picture = document.getElementById("imageUploaded")
+
+    let title = document.getElementById('workTitle').value
+
+    let category = document.getElementById('categoryOptions').value
+
+    // Si formulaire incomplet
+    if (image === '' || title === '' || category === 'null') {
+        console.log('error')
+        return false
+    }
+    let formData = new FormData();
+    formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", category);
+
+    const addReponse = await addWorks(formData)
+    //reset du formulaire
+    let templateAddModal = `<figure id="${addReponse.id}"  class="adminWorks">
+                    <img src="${url}" alt="${title}">
+                    <button class="trash"><i class="fa-solid fa-trash-can"></i></button>
+                    </figure>`;
+
+    let templateAddHome = `<figure id="${category}" data-pictureId="${addReponse.id}" class="show ">
+                                <img src="${url}" alt="${title}">
+                                <figcaption>${title}</figcaption>
+                                </figure>`;
+
+    document.querySelector(".gallery").insertAdjacentHTML("beforeend", templateAddHome)
+    document.querySelector("#adminGallery").insertAdjacentHTML("beforeend", templateAddModal)
+    document.querySelector("#adminGallery").lastElementChild.lastElementChild.addEventListener('click', workDelete)
+    document.querySelector(".formAddWorks").reset()
+    document.getElementById('pictureLoaded').classList.remove('hiddenModalPart')
+    document.querySelector('.addImgLoader i').classList.remove('hiddenModalPart')
+    document.querySelector('.addImgLoader p').classList.remove('hiddenModalPart')
+    document.getElementById('btnAddContent').classList.replace('accessible', 'inaccessible')
+    document.getElementById('btnAddContent').setAttribute('disabled', true);
+    picture.src = "#"
+})
 
 //changement de couleur/curseur du bouton d'envoi de projet
 let formAdd = document.querySelector('.formAddWorks')
@@ -192,19 +241,19 @@ formAdd.addEventListener('change', () => {
     let image = document.getElementById('pictureLoaded').files[0]
 
     let title = document.getElementById('workTitle').value
-    
+
     let category = document.getElementById('categoryOptions').value
-    
+
     let btnformAdd = document.querySelector('#btnAddContent')
     if (image === undefined || title === '' || category === 'null') {
-        btnformAdd.classList.replace('accessible','inaccessible')
+        btnformAdd.classList.replace('accessible', 'inaccessible')
         document.querySelector('#btnAddContent').setAttribute('disabled', true)
     } else {
-        btnformAdd.classList.replace('inaccessible','accessible')
+        btnformAdd.classList.replace('inaccessible', 'accessible')
         document.querySelector('#btnAddContent').removeAttribute('disabled')
     }
     //cache le bouton lorsqu'une image est selectionné.
-    if( image !== undefined){
+    if (image !== undefined) {
         document.getElementById('pictureLoaded').classList.add('hiddenModalPart');
         document.querySelector('.addImgLoader i').classList.add('hiddenModalPart')
         document.querySelector('.addImgLoader p').classList.add('hiddenModalPart')
